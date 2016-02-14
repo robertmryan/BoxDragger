758fdf83f90cd9b8aa1b233c4e66162df284e208
Robert M. Ryan <robert.ryan@mindspring.com>
2016-02-14 01:21:43 -0800

    Initial Commit

---
+++ BoxDragger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoxDragger
+//
+//  Created by Robert Ryan on 2/14/16.
+//  Copyright © 2016 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
